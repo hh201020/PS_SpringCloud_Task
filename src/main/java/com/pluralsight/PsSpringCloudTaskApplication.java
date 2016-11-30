3f542ab4b6666d6885f636af76dd3ba73ce05b38
Jerry Hu <hh201020@gmail.com>
2016-11-30 13:33:07 -0500

    Create a simple task to run by loading bean.

--- src/main/java/com/pluralsight/PsSpringCloudTaskApplication.java
+++ src/main/java/com/pluralsight/PsSpringCloudTaskApplication.java
@@ -13,5 +13,18 @@ public class PsSpringCloudTaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PsSpringCloudTaskApplication.class, args);
 	}
-
+	
+	@Bean
+	public TollProcessingTask tollProcessingTask() {
+		return new TollProcessingTask();
+	}
+	
+	public class TollProcessingTask implements CommandLineRunner {
+		@Override
+		public void run(String... strings) throws Exception {
+			if(null != strings) {
+				System.out.println("Parameter length is " + strings.length + " its first string is: " + strings[0]);
+			}
+		}
+	}
 }
