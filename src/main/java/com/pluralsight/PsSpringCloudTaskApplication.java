b59bf6f99ce38dd72b9fa6aba4f31d37bb235868
Jerry Hu <hh201020@gmail.com>
2016-11-30 13:31:50 -0500

    First Commit

---
+++ src/main/java/com/pluralsight/PsSpringCloudTaskApplication.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableTask
+public class PsSpringCloudTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsSpringCloudTaskApplication.class, args);
+	}
+
+}
